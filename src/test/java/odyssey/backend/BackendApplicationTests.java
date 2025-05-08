71ee498db1ddf3921ce606fca60e93461d46b42c
hsm <hll2071@naver.com>
2025-05-08 19:02:20 +0900

    initial commit

---
+++ src/test/java/odyssey/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package odyssey.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
