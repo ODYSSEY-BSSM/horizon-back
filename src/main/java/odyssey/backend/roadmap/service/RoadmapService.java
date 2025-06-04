4e015c27112a41d373601a74182cb4814a6f1e3f
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-04 23:24:26 +0900

    feat(#1) : 로드맵에 이미지 업로드 및 이미지, 노드삭제 기능 추가
    로드맵 생성 시 썸네일 이미지 업로드 로직을 추가하고,
    삭제 시 연관된 이미지와 노드도 함께 삭제되도록 변경함.

--- src/main/java/odyssey/backend/roadmap/service/RoadmapService.java
+++ src/main/java/odyssey/backend/roadmap/service/RoadmapService.java
@@ -1,12 +1,17 @@
 package odyssey.backend.roadmap.service;
 
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.image.domain.Image;
+import odyssey.backend.image.domain.ImageRepository;
+import odyssey.backend.image.service.ImageService;
+import odyssey.backend.node.domain.NodeRepository;
 import odyssey.backend.roadmap.domain.RoadmapRepository;
 import odyssey.backend.roadmap.dto.RoadmapRequest;
 import odyssey.backend.roadmap.dto.RoadmapResponse;
 import org.springframework.stereotype.Service;
 import odyssey.backend.roadmap.domain.Roadmap;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,24 +20,37 @@ import java.util.List;
 public class RoadmapService {
 
     private final RoadmapRepository roadmapRepository;
+    private final ImageService imageService;
+    private final ImageRepository imageRepository;
+    private final NodeRepository nodeRepository;
 
     public List<RoadmapResponse> findAllRoadmaps() {
         return roadmapRepository.findAll().stream()
-                .map(RoadmapResponse::new)
+                .map(roadmap -> {
+                    Image image = imageRepository.findByRoadmapId(roadmap.getId());
+                    return new RoadmapResponse(roadmap, image.getUrl());
+                })
                 .toList();
     }
 
-    public RoadmapResponse save(RoadmapRequest request) {
-        Roadmap roadmap = roadmapRepository.save(Roadmap.builder()
-                .title(request.getTitle())
-                .description(request.getDescription())
-                .categories(request.getCategories())
-                .build());
+    public RoadmapResponse save(RoadmapRequest request, MultipartFile thumbnail) {
+        Roadmap roadmap = roadmapRepository.save(
+                Roadmap.builder()
+                        .title(request.getTitle())
+                        .description(request.getDescription())
+                        .categories(request.getCategories())
+                        .build()
+        );
 
-        return new RoadmapResponse(roadmap);
+        Image image = imageService.save(thumbnail, roadmap);
+
+        return new RoadmapResponse(roadmap, image.getUrl());
     }
 
+    @Transactional
     public void deleteRoadmapById(Long id) {
+        imageRepository.deleteByRoadmapId(id);
+        nodeRepository.deleteByRoadmapId(id);
         roadmapRepository.deleteById(id);
     }
 
@@ -43,8 +61,7 @@ public class RoadmapService {
 
         roadmap.update(request);
 
-        return new RoadmapResponse(roadmap);
+        Image image = imageRepository.findByRoadmapId(roadmap.getId());
+        return new RoadmapResponse(roadmap, image.getUrl());
     }
-
-
 }
