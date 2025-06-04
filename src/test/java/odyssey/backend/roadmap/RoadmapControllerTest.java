8833f4a6b4390004a092b0c0049ba49e03150fea
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-04 23:38:28 +0900

    test(#1) : 이미지 업로드 테스트 코드 작성
    서비스 로직에 이미지 업로드가 추가됨에 따라 테스트코드에도 반영

--- src/test/java/odyssey/backend/roadmap/RoadmapControllerTest.java
+++ src/test/java/odyssey/backend/roadmap/RoadmapControllerTest.java
@@ -5,7 +5,9 @@ import odyssey.backend.roadmap.dto.RoadmapRequest;
 import odyssey.backend.roadmap.dto.RoadmapResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,23 +23,31 @@ class RoadmapControllerTest extends ControllerTest {
     @WithMockUser
     @Test
     void 로드맵을_생성한다() throws Exception {
-
         RoadmapRequest request = new RoadmapRequest("자바자바", "조아요", List.of("백엔드", "스프링"));
+        RoadmapResponse fakeResponse = new RoadmapResponse(1L, request.getTitle(), request.getDescription(), request.getCategories(), "fake-url");
 
-        RoadmapResponse fakeResponse = new RoadmapResponse(
-                1L,
-                request.getTitle(),
-                request.getDescription(),
-                request.getCategories()
+        MockMultipartFile roadmapPart = new MockMultipartFile(
+                "roadmap",
+                "",
+                "application/json",
+                objectMapper.writeValueAsBytes(request)
         );
 
-        given(roadmapService.save(any(RoadmapRequest.class)))
+        MockMultipartFile thumbnail = new MockMultipartFile(
+                "thumbnail",
+                "image.jpg",
+                "image/jpeg",
+                "<<fake image content>>".getBytes()
+        );
+
+        given(roadmapService.save(any(RoadmapRequest.class), any(MultipartFile.class)))
                 .willReturn(fakeResponse);
 
-        String responseBody = mvc.perform(post("/roadmap/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-                        .with(csrf()))
+        String responseBody = mvc.perform(multipart("/roadmap/create")
+                        .file(roadmapPart)
+                        .file(thumbnail)
+                        .with(csrf())
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
@@ -47,11 +57,16 @@ class RoadmapControllerTest extends ControllerTest {
         System.out.println("응답 결과: " + responseBody);
     }
 
+
     @WithMockUser
     @Test
     void 로드맵을_전체조회한다() throws Exception {
-        RoadmapResponse response1 = new RoadmapResponse(1L, "타이틀1", "설명1", List.of("테스트1", "테스트2"));
-        RoadmapResponse response2 = new RoadmapResponse(2L, "타이틀2", "설명2", List.of("테스트1", "테스트2"));
+        RoadmapResponse response1 = new RoadmapResponse(
+                1L, "타이틀1", "설명1", List.of("테스트1", "테스트2"), "https://image1.com"
+        );
+        RoadmapResponse response2 = new RoadmapResponse(
+                2L, "타이틀2", "설명2", List.of("테스트3", "테스트4"), "https://image2.com"
+        );
 
         given(roadmapService.findAllRoadmaps()).willReturn(List.of(response1, response2));
 
@@ -65,14 +80,25 @@ class RoadmapControllerTest extends ControllerTest {
         System.out.println("전체 조회 응답: " + responseBody);
     }
 
+
     @WithMockUser
     @Test
     void 로드맵을_수정한다() throws Exception {
         Long roadmapId = 1L;
-        RoadmapRequest request = new RoadmapRequest("수정된 타이틀", "수정된 설명", List.of("수정된 테스트1", "수정된 테스트2"));
-        RoadmapResponse fakeResponse = new RoadmapResponse(roadmapId, request.getTitle(), request.getDescription(), request.getCategories());
+        RoadmapRequest request = new RoadmapRequest(
+                "수정된 타이틀", "수정된 설명", List.of("수정된 테스트1", "수정된 테스트2")
+        );
 
-        given(roadmapService.update(eq(roadmapId), any(RoadmapRequest.class))).willReturn(fakeResponse);
+        RoadmapResponse fakeResponse = new RoadmapResponse(
+                roadmapId,
+                request.getTitle(),
+                request.getDescription(),
+                request.getCategories(),
+                "https://updatedThumbnail.com"
+        );
+
+        given(roadmapService.update(eq(roadmapId), any(RoadmapRequest.class)))
+                .willReturn(fakeResponse);
 
         String responseBody = mvc.perform(put("/roadmap/update/{id}", roadmapId)
                         .contentType(MediaType.APPLICATION_JSON)
