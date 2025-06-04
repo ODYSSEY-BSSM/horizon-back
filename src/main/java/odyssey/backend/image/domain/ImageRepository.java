47a5d51e4089439e9ae5f7a6d0fcff935232560c
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-04 23:25:58 +0900

    feat(#1) : 로드맵 삭제 시 이미지도 삭제되게 메서드 작성
    로드맵 삭제 로직에서 로드맵이 삭제되면 이미지도 삭제되어야하기 때문에 작성

--- src/main/java/odyssey/backend/image/domain/ImageRepository.java
+++ src/main/java/odyssey/backend/image/domain/ImageRepository.java
@@ -3,9 +3,8 @@ package odyssey.backend.image.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
-    Optional<Image> findByRoadmapId(Long roadmapId);
+    Image findByRoadmapId(Long roadmapId);
+    void deleteByRoadmapId(Long roadmapId);
 }
