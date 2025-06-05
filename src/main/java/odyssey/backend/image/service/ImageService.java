a08f627cbc5d866b7f9320e659bbdd0b66145ce6
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-05 10:25:13 +0900

    feat(#1) : Image 검증
    ImageService 에서 파일이 이미지 인지 검증하도록 수정

--- src/main/java/odyssey/backend/image/service/ImageService.java
+++ src/main/java/odyssey/backend/image/service/ImageService.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -25,6 +26,10 @@ public class ImageService {
     private final ImageRepository imageRepository;
 
     public Image save(MultipartFile file, Roadmap roadmap) {
+        if (!Objects.requireNonNull(file.getContentType()).startsWith("image/")) {
+            throw new IllegalArgumentException("이미지 파일만 업로드 가능합니다.");
+        }
+
         try {
             Path uploadPath = Paths.get(thumbnailDir).toAbsolutePath();
             if (Files.notExists(uploadPath)) {
