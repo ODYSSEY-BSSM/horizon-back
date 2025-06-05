fcf8fa238a5ea156048dd709b5fe135bee02aa04
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-05 12:08:33 +0900

    feat(#1) : 마지막으로 접속한 로드맵찾는 메서드 추가
    마지막으로 접속한 로드맵을 찾는 JPA 메서드가 필요해짐에 따라 추가

--- src/main/java/odyssey/backend/roadmap/domain/RoadmapRepository.java
+++ src/main/java/odyssey/backend/roadmap/domain/RoadmapRepository.java
@@ -3,4 +3,5 @@ package odyssey.backend.roadmap.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+    Roadmap findTopByOrderByLastAccessedAtDesc();
 }
