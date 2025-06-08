a82649dd886c85c473f47fc7cd1d620b48efb7f9
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-09 08:11:32 +0900

    refactor(#6) : Optional 처리
    마지막 접속 로드맵을 찾는 메서드에 예외처리가 필요하여
    Optional 로 처리

--- src/main/java/odyssey/backend/roadmap/domain/RoadmapRepository.java
+++ src/main/java/odyssey/backend/roadmap/domain/RoadmapRepository.java
@@ -2,6 +2,8 @@ package odyssey.backend.roadmap.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
-    Roadmap findTopByOrderByLastAccessedAtDesc();
+    Optional<Roadmap> findTopByOrderByLastAccessedAtDesc();
 }
