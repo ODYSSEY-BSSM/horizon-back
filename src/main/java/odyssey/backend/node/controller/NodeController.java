cc9ecb6fd5cf56c7fb998349594b6cbacc53b762
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-18 00:51:55 +0900

    refactor(#23) : 공통 Response 작성
    일관성 있는 응답을 위해 공통 Response 으로 수정

--- src/main/java/odyssey/backend/node/controller/NodeController.java
+++ src/main/java/odyssey/backend/node/controller/NodeController.java
@@ -2,12 +2,14 @@ package odyssey.backend.node.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import odyssey.backend.global.response.CommonResponse;
+import odyssey.backend.global.response.ListCommonResponse;
+import odyssey.backend.global.response.SingleCommonResponse;
 import odyssey.backend.node.dto.NodeRequest;
 import odyssey.backend.node.dto.NodeResponse;
 import odyssey.backend.node.service.NodeService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,31 +19,36 @@ public class NodeController {
     private final NodeService nodeService;
 
     @PostMapping
-    public NodeResponse createNode(
-            @PathVariable Long roadmapId, @Valid @RequestBody NodeRequest nodeRequest) {
-        return nodeService.createNode(roadmapId, nodeRequest);
+    public SingleCommonResponse<NodeResponse> createNode(
+            @PathVariable Long roadmapId,
+            @Valid @RequestBody NodeRequest nodeRequest) {
+        return CommonResponse.ok(nodeService.createNode(roadmapId, nodeRequest));
     }
 
     @GetMapping
-    public List<NodeResponse> getNodesByRoadmap(@PathVariable Long roadmapId) {
-        return nodeService.getNodesByRoadmapId(roadmapId);
+    public ListCommonResponse<NodeResponse> getNodesByRoadmap(@PathVariable Long roadmapId) {
+        return CommonResponse.ok(nodeService.getNodesByRoadmapId(roadmapId));
     }
 
     @GetMapping("/{nodeId}")
-    public NodeResponse getNode(@PathVariable Long nodeId ,@PathVariable Long roadmapId) {
-        return nodeService.getNodeByIdAndRoadmapId(nodeId, roadmapId);
+    public SingleCommonResponse<NodeResponse> getNode(
+            @PathVariable Long nodeId,
+            @PathVariable Long roadmapId) {
+        return CommonResponse.ok(nodeService.getNodeByIdAndRoadmapId(nodeId, roadmapId));
     }
 
     @PutMapping("/{nodeId}")
-    public NodeResponse updateNode(
-            @PathVariable Long nodeId, @PathVariable Long roadmapId, @Valid @RequestBody NodeRequest request
-    ){
-        return nodeService.updateNode(nodeId, roadmapId, request);
+    public SingleCommonResponse<NodeResponse> updateNode(
+            @PathVariable Long nodeId,
+            @PathVariable Long roadmapId,
+            @Valid @RequestBody NodeRequest request) {
+        return CommonResponse.ok(nodeService.updateNode(nodeId, roadmapId, request));
     }
 
     @DeleteMapping("/{nodeId}")
-    public void deleteNode(@PathVariable Long nodeId, @PathVariable Long roadmapId) {
+    public void deleteNode(
+            @PathVariable Long nodeId,
+            @PathVariable Long roadmapId) {
         nodeService.deleteNodeByIdAndRoadmapId(nodeId, roadmapId);
     }
-
 }
