5717c0480451c3378adce33c4348ffa4bc1b45a7
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-27 10:42:07 +0900

    refactor(#41) : 예외처리 enum 화
    Http Status 와 message 를 enum 클래스로 변경

---
+++ src/main/java/odyssey/backend/global/exception/ExecptionCode.java
@@ -0,0 +1,19 @@
+package odyssey.backend.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExecptionCode {
+    ROADMAP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 로드맵입니다."),
+    NODE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 노드입니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ExecptionCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
