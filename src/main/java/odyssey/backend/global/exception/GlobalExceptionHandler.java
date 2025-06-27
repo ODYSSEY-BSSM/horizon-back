44e2aa4b9392a134da14c8a9e2f1636db634860c
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-27 10:42:20 +0900

    refactor(#41) : 예외처리 enum 화
    코드 반영

--- src/main/java/odyssey/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/odyssey/backend/global/exception/GlobalExceptionHandler.java
@@ -1,24 +1,24 @@
 package odyssey.backend.global.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(RoadmapNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleRoadmapNotFoundException(RoadmapNotFoundException e) {
-        return buildResponse(HttpStatus.NOT_FOUND, "존재하지 않는 로드맵입니다.");
+        return buildResponse(ExecptionCode.ROADMAP_NOT_FOUND);
     }
 
     @ExceptionHandler(NodeNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNodeNotFoundException(NodeNotFoundException e) {
-        return buildResponse(HttpStatus.NOT_FOUND, "존재하지 않는 노드입니다.");
+        return buildResponse(ExecptionCode.NODE_NOT_FOUND);
     }
 
-    private ResponseEntity<ExceptionResponse> buildResponse(HttpStatus status, String message) {
-        return new ResponseEntity<>(new ExceptionResponse(status.value(), message), status);
+    private ResponseEntity<ExceptionResponse> buildResponse(ExecptionCode execptionCode) {
+        return ResponseEntity
+                .status(execptionCode.getStatus())
+                .body(new ExceptionResponse(execptionCode.getStatus().value(), execptionCode.getMessage()));
     }
 }
