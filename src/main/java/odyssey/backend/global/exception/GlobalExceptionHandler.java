3851589b608d7a27a2701ef6e75b31526225f8d0
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-30 17:05:25 +0900

    feat(#45) : ExceptionHandler 수정
    잘못된 요청 핸들러 수정

--- src/main/java/odyssey/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/odyssey/backend/global/exception/GlobalExceptionHandler.java
@@ -24,7 +24,7 @@ public class GlobalExceptionHandler {
                 .get(0)
                 .getDefaultMessage();
 
-        return buildResponse(ExceptionCode.BAD_REQUEST, errorMessage);
+        return buildResponse(ExceptionCode.INVALID_REQUEST, errorMessage);
     }
 
     private ResponseEntity<ExceptionResponse> buildResponse(ExceptionCode exceptionCode) {
