15aa941f563538615d3fa1a254615968fab57d64
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-30 19:11:01 +0900

    feat(#48) : 디렉토리 레포지토리
    디렉토리 레포지토리 작성

---
+++ src/main/java/odyssey/backend/directory/domain/DirectoryRepository.java
@@ -0,0 +1,6 @@
+package odyssey.backend.directory.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectoryRepository extends JpaRepository<Directory, Long> {
+}
