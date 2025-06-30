9fdcb8f62ddff1ae12440c495ceea520e561115c
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-01 08:36:54 +0900

    feat(#48) : 디렉토리 컨트롤러
    디렉토리 컨트롤러 작성

---
+++ src/main/java/odyssey/backend/directory/controller/DirectoryController.java
@@ -0,0 +1,31 @@
+package odyssey.backend.directory.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.directory.dto.DirectoryRequest;
+import odyssey.backend.directory.dto.DirectoryResponse;
+import odyssey.backend.directory.service.DirectoryService;
+import odyssey.backend.global.response.CommonResponse;
+import odyssey.backend.global.response.ListCommonResponse;
+import odyssey.backend.global.response.SingleCommonResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/directories")
+public class DirectoryController {
+
+    private final DirectoryService directoryService;
+
+    @PostMapping("/create")
+    public SingleCommonResponse<DirectoryResponse> createDirectory(
+            @Valid @RequestBody DirectoryRequest request
+    ){
+        return CommonResponse.ok(directoryService.createDirectory(request));
+    }
+
+    @GetMapping("/getAll")
+    public ListCommonResponse<DirectoryResponse> getAll() {
+        return CommonResponse.ok(directoryService.getAllDirectories());
+    }
+}
