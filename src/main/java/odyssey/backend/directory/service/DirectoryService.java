ed076a7442023d020cf97781ec2bfd0b02c1fedf
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-01 08:49:09 +0900

    feat(#48) : 디렉토리 서비스
    디렉토리 서비스 코드 작성

---
+++ src/main/java/odyssey/backend/directory/service/DirectoryService.java
@@ -0,0 +1,47 @@
+package odyssey.backend.directory.service;
+
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.directory.domain.Directory;
+import odyssey.backend.directory.domain.DirectoryRepository;
+import odyssey.backend.directory.dto.DirectoryRequest;
+import odyssey.backend.directory.dto.DirectoryResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DirectoryService {
+
+    private final DirectoryRepository directoryRepository;
+
+    public DirectoryResponse createDirectory(DirectoryRequest directoryRequest) {
+        Directory parent = null;
+
+        if (directoryRequest.getParentId() != null) {
+            parent = directoryRepository.findById(directoryRequest.getParentId())
+                    .orElseThrow(() -> new IllegalArgumentException(""));
+        }
+
+        Directory directory = Directory.builder()
+                .name(directoryRequest.getName())
+                .parent(parent)
+                .build();
+
+        directoryRepository.save(directory);
+
+        return new DirectoryResponse(directory);
+    }
+
+    public List<DirectoryResponse> getAllDirectories() {
+        return directoryRepository.findAll().stream()
+                .filter(directory -> directory.getParent() == null)
+                .map(DirectoryResponse::new)
+                .toList();
+    }
+
+    public Directory findDirectoryById(Long id) {
+        return directoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("디렉토리를 찾을 수 없습니다."));
+    }
+}
--- src/main/java/odyssey/backend/roadmap/service/RoadmapService.java
+++ src/main/java/odyssey/backend/roadmap/service/RoadmapService.java
@@ -1,114 +1,120 @@
-package odyssey.backend.roadmap.service;
+    package odyssey.backend.roadmap.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import odyssey.backend.global.exception.RoadmapNotFoundException;
-import odyssey.backend.image.domain.Image;
-import odyssey.backend.image.service.ImageService;
-import odyssey.backend.node.domain.NodeRepository;
-import odyssey.backend.roadmap.domain.RoadmapRepository;
-import odyssey.backend.roadmap.dto.RoadmapCountResponse;
-import odyssey.backend.roadmap.dto.RoadmapRequest;
-import odyssey.backend.roadmap.dto.RoadmapResponse;
-import org.springframework.stereotype.Service;
-import odyssey.backend.roadmap.domain.Roadmap;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+    import lombok.RequiredArgsConstructor;
+    import lombok.extern.slf4j.Slf4j;
+    import odyssey.backend.directory.domain.Directory;
+    import odyssey.backend.directory.service.DirectoryService;
+    import odyssey.backend.global.exception.RoadmapNotFoundException;
+    import odyssey.backend.image.domain.Image;
+    import odyssey.backend.image.service.ImageService;
+    import odyssey.backend.roadmap.domain.RoadmapRepository;
+    import odyssey.backend.roadmap.dto.RoadmapCountResponse;
+    import odyssey.backend.roadmap.dto.RoadmapRequest;
+    import odyssey.backend.roadmap.dto.RoadmapResponse;
+    import org.springframework.stereotype.Service;
+    import odyssey.backend.roadmap.domain.Roadmap;
+    import org.springframework.transaction.annotation.Transactional;
+    import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+    import java.util.List;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class RoadmapService {
+    @Service
+    @Slf4j
+    @RequiredArgsConstructor
+    public class RoadmapService {
 
-    private final RoadmapRepository roadmapRepository;
-    private final ImageService imageService;
-    private final NodeRepository nodeRepository;
+        private final RoadmapRepository roadmapRepository;
+        private final ImageService imageService;
+        private final DirectoryService directoryService;
 
-    public List<RoadmapResponse> findAllRoadmaps() {
-        return roadmapRepository.findAll().stream()
-                .map(roadmap -> {
-                    Image image = imageService.getImageByRoadmap(roadmap);
-                    return new RoadmapResponse(roadmap, image.getUrl());
-                })
-                .toList();
+
+        public List<RoadmapResponse> findAllRoadmaps() {
+            return roadmapRepository.findAll().stream()
+                    .map(roadmap -> {
+                        Image image = imageService.getImageByRoadmap(roadmap);
+                        return new RoadmapResponse(roadmap, image.getUrl());
+                    })
+                    .toList();
+        }
+
+        @Transactional
+        public RoadmapResponse save(RoadmapRequest request, MultipartFile thumbnail) {
+
+            Directory directory = directoryService.findDirectoryById(request.getDirectoryId());
+
+            Roadmap roadmap = roadmapRepository.save(
+                    Roadmap.builder()
+                            .title(request.getTitle())
+                            .description(request.getDescription())
+                            .categories(request.getCategories())
+                            .directory(directory)
+                            .build()
+            );
+
+            roadmap.updateLastModifiedAt();
+
+            Image image = imageService.save(thumbnail, roadmap);
+
+            log.info("생성된 로드맵 Id : {}", roadmap.getId());
+
+            return new RoadmapResponse(roadmap, image.getUrl());
+        }
+
+        @Transactional
+        public void deleteRoadmapById(Long id) {
+
+            log.info("삭제된 로드맵 Id : {}", id);
+
+            roadmapRepository.deleteById(id);
+
+        }
+
+        @Transactional
+        public RoadmapResponse update(Long id, RoadmapRequest request) {
+            Roadmap roadmap = roadmapRepository.findById(id)
+                    .orElseThrow(RoadmapNotFoundException::new);
+
+            roadmap.update(request.getTitle(), request.getDescription(), request.getCategories());
+
+            roadmap.updateLastModifiedAt();
+
+            log.info("업데이트 요청 로드맵 Id : {}", roadmap.getId());
+
+            Image image = imageService.getImageByRoadmap(roadmap);
+
+            return new RoadmapResponse(roadmap, image.getUrl());
+        }
+
+        @Transactional
+        public RoadmapResponse toggleFavorite(Long id){
+            Roadmap roadmap = roadmapRepository.findById(id)
+                    .orElseThrow(RoadmapNotFoundException::new);
+
+            roadmap.toggleFavorite();
+
+            log.info("즐겨찾기 요청 로드맵 Id : {}", roadmap.getId());
+
+            Image image = imageService.getImageByRoadmap(roadmap);
+
+            return new RoadmapResponse(roadmap, image.getUrl());
+        }
+
+        public RoadmapResponse getLastAccessedRoadmap() {
+
+            Roadmap roadmap = roadmapRepository.findTopByOrderByLastAccessedAtDesc()
+                    .orElseThrow(RoadmapNotFoundException::new);
+
+            Image image = imageService.getImageByRoadmap(roadmap);
+
+            log.info("마지막 접속 로드맵 Id : {}", roadmap.getId());
+
+            return new RoadmapResponse(roadmap, image.getUrl());
+        }
+
+        public RoadmapCountResponse getRoadmapCount(){
+            Long count = roadmapRepository.count();
+
+            return new RoadmapCountResponse(count);
+        }
+
     }
-
-    @Transactional
-    public RoadmapResponse save(RoadmapRequest request, MultipartFile thumbnail) {
-        Roadmap roadmap = roadmapRepository.save(
-                Roadmap.builder()
-                        .title(request.getTitle())
-                        .description(request.getDescription())
-                        .categories(request.getCategories())
-                        .build()
-        );
-
-        roadmap.updateLastModifiedAt();
-
-        Image image = imageService.save(thumbnail, roadmap);
-
-        log.info("생성된 로드맵 Id : {}", roadmap.getId());
-
-        return new RoadmapResponse(roadmap, image.getUrl());
-    }
-
-    @Transactional
-    public void deleteRoadmapById(Long id) {
-      
-        log.info("삭제된 로드맵 Id : {}", id);
-
-        roadmapRepository.deleteById(id);
-
-    }
-
-    @Transactional
-    public RoadmapResponse update(Long id, RoadmapRequest request) {
-        Roadmap roadmap = roadmapRepository.findById(id)
-                .orElseThrow(RoadmapNotFoundException::new);
-
-        roadmap.update(request.getTitle(), request.getDescription(), request.getCategories());
-
-        roadmap.updateLastModifiedAt();
-
-        log.info("업데이트 요청 로드맵 Id : {}", roadmap.getId());
-
-        Image image = imageService.getImageByRoadmap(roadmap);
-
-        return new RoadmapResponse(roadmap, image.getUrl());
-    }
-
-    @Transactional
-    public RoadmapResponse toggleFavorite(Long id){
-        Roadmap roadmap = roadmapRepository.findById(id)
-                .orElseThrow(RoadmapNotFoundException::new);
-
-        roadmap.toggleFavorite();
-
-        log.info("즐겨찾기 요청 로드맵 Id : {}", roadmap.getId());
-
-        Image image = imageService.getImageByRoadmap(roadmap);
-
-        return new RoadmapResponse(roadmap, image.getUrl());
-    }
-
-    public RoadmapResponse getLastAccessedRoadmap() {
-
-        Roadmap roadmap = roadmapRepository.findTopByOrderByLastAccessedAtDesc()
-                .orElseThrow(RoadmapNotFoundException::new);
-
-        Image image = imageService.getImageByRoadmap(roadmap);
-
-        log.info("마지막 접속 로드맵 Id : {}", roadmap.getId());
-
-        return new RoadmapResponse(roadmap, image.getUrl());
-    }
-
-    public RoadmapCountResponse getRoadmapCount(){
-        Long count = roadmapRepository.count();
-
-        return new RoadmapCountResponse(count);
-    }
-
-}
