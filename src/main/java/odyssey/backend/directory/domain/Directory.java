a228a04bab951173f8c7abf893accdd624ebd43c
leegunwoooo <leegunwoo0325@gmail.com>
2025-06-30 19:09:15 +0900

    feat(#48) : 디렉토리 엔티티
    티렉토리 엔티티

---
+++ src/main/java/odyssey/backend/directory/domain/Directory.java
@@ -0,0 +1,32 @@
+package odyssey.backend.directory.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import odyssey.backend.roadmap.domain.Roadmap;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "directory_tbl")
+public class Directory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dir_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Directory parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Directory> children;
+
+    @OneToMany(mappedBy = "directory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Roadmap> roadmaps;
+
+}
