40b357112ece79604d8a5c739c90a4dd3e9ac756
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-01 17:54:47 +0900

    feat(#48) : 루트 로드맵 컨트롤러
    컨트롤러에 루트 로드맵도 포함한 response 반환

--- src/main/java/odyssey/backend/directory/controller/DirectoryController.java
+++ src/main/java/odyssey/backend/directory/controller/DirectoryController.java
@@ -4,9 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import odyssey.backend.directory.dto.DirectoryRequest;
 import odyssey.backend.directory.dto.DirectoryResponse;
+import odyssey.backend.directory.dto.RootContentResponse;
 import odyssey.backend.directory.service.DirectoryService;
 import odyssey.backend.global.response.CommonResponse;
-import odyssey.backend.global.response.ListCommonResponse;
 import odyssey.backend.global.response.SingleCommonResponse;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,16 +24,6 @@ public class DirectoryController {
         return CommonResponse.ok(directoryService.createDirectory(request));
     }
 
-    @GetMapping("/getAll")
-    public ListCommonResponse<DirectoryResponse> getAll() {
-        return CommonResponse.ok(directoryService.getAllDirectories());
-    }
-
-    @GetMapping("/{id}")
-    public SingleCommonResponse<DirectoryResponse> getDirectoryById(@PathVariable Long id) {
-        return CommonResponse.ok(directoryService.getDirectory(id));
-    }
-
     @PutMapping("/{id}")
     public SingleCommonResponse<DirectoryResponse> updateDirectory(
             @PathVariable Long id,
@@ -44,5 +34,11 @@ public class DirectoryController {
     @DeleteMapping("/{id}")
     public void deleteDirectory(@PathVariable Long id) {
         directoryService.deleteDirectory(id);
+    }
+
+    @GetMapping("/root-contents")
+    public SingleCommonResponse<RootContentResponse> getRootContents() {
+        RootContentResponse response = directoryService.getRootContents();
+        return CommonResponse.ok(response);
     }
 }
