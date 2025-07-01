3a613516baa8229e8563a66516d20dd154b3a411
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-01 17:56:02 +0900

    feat(#48) : 루트 디렉토리
    루트 디렉토리를 분리하기 위해 부모가 Null 인 디렉토리 리스트를 반환하는 메서드 추가

--- src/main/java/odyssey/backend/directory/domain/DirectoryRepository.java
+++ src/main/java/odyssey/backend/directory/domain/DirectoryRepository.java
@@ -2,5 +2,8 @@ package odyssey.backend.directory.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DirectoryRepository extends JpaRepository<Directory, Long> {
+    List<Directory> findByParentIsNull();
 }
