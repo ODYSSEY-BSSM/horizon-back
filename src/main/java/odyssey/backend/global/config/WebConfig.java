699af31a9ae235263824fa8dec5f58e769be0799
hsm <hll2071@naver.com>
2025-07-08 17:59:48 +0900

    feat(#59) : CORS 세팅
    배포 후 프론트엔드와의 연동을 위해서 CORS 세팅을 추가

--- src/main/java/odyssey/backend/global/config/WebConfig.java
+++ src/main/java/odyssey/backend/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package odyssey.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/thumbnails/**")
                 .addResourceLocations("classpath:/static/uploads/thumbnails/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5174") // 허용할 도메인 설정
+                .allowedMethods("*") // 허용할 HTTP 메소드 설정
+                .allowedHeaders("*") // 허용할 헤더 설정
+                .allowCredentials(true) // 인증정보 허용 여부
+                .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
