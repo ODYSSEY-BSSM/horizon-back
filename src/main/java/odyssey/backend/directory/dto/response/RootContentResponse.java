e0c017c77db6d1d0db832f17a1b1bdca49b4963e
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-20 23:56:07 +0900

    refactor(#94) : 정적 팩토리 메서드
    캡슐화를 위해 변경

--- src/main/java/odyssey/backend/directory/dto/response/RootContentResponse.java
+++ src/main/java/odyssey/backend/directory/dto/response/RootContentResponse.java
@@ -1,14 +1,22 @@
 package odyssey.backend.directory.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import odyssey.backend.roadmap.dto.response.SimpleRoadmapResponse;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class RootContentResponse {
-    private List<DirectoryResponse> directories;
-    private List<SimpleRoadmapResponse> roadmaps;
+    private final List<DirectoryResponse> directories;
+    private final List<SimpleRoadmapResponse> roadmaps;
+
+    public static RootContentResponse from(List<DirectoryResponse> directories, List<SimpleRoadmapResponse> roadmaps) {
+        return new RootContentResponse(directories, roadmaps);
+    }
+
+    RootContentResponse(List<DirectoryResponse> directories,
+                        List<SimpleRoadmapResponse> roadmaps) {
+        this.directories = directories;
+        this.roadmaps = roadmaps;
+    }
 }
