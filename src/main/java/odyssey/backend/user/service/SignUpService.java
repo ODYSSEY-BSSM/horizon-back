b185e7c902310d4e1b67e09172f2c7eb04bd7ed2
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-06 05:10:35 +0900

    feat(#81) : 회원가입 서비스 클래스
    회원가입을 위한 Service 클래스를 작성하였습니다.

---
+++ src/main/java/odyssey/backend/user/service/SignUpService.java
@@ -0,0 +1,20 @@
+package odyssey.backend.user.service;
+
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.user.Repository.UserRepository;
+import odyssey.backend.user.domain.User;
+import odyssey.backend.user.dto.request.SignUpRequest;
+import odyssey.backend.user.dto.response.SignUpResponse;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SignUpService {
+
+    private final UserRepository userRepository;
+
+    public SignUpResponse signUp(SignUpRequest request) {
+        return SignUpResponse.from(userRepository.save(User.from(request)));
+    }
+
+}
