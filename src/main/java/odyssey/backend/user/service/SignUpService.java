2abfe8eb7d7a207e81b8b40cec7c9742f3105d31
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-09 17:55:11 +0900

    refactor(#99) : 비밀번호 암호화

--- src/main/java/odyssey/backend/user/service/SignUpService.java
+++ src/main/java/odyssey/backend/user/service/SignUpService.java
@@ -1,20 +1,27 @@
-package odyssey.backend.user.service;
+    package odyssey.backend.user.service;
 
-import lombok.RequiredArgsConstructor;
-import odyssey.backend.user.domain.UserRepository;
-import odyssey.backend.user.domain.User;
-import odyssey.backend.user.dto.request.SignUpRequest;
-import odyssey.backend.user.dto.response.SignUpResponse;
-import org.springframework.stereotype.Service;
+    import lombok.RequiredArgsConstructor;
+    import odyssey.backend.user.domain.UserRepository;
+    import odyssey.backend.user.domain.User;
+    import odyssey.backend.user.dto.request.SignUpRequest;
+    import odyssey.backend.user.dto.response.SignUpResponse;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class SignUpService {
+    @RequiredArgsConstructor
+    @Service
+    public class SignUpService {
 
-    private final UserRepository userRepository;
+        private final UserRepository userRepository;
+        private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public SignUpResponse signUp(SignUpRequest request) {
-        return SignUpResponse.from(userRepository.save(User.from(request)));
+        public SignUpResponse signUp(SignUpRequest request) {
+            User user = userRepository.save(User.from(
+                    request,
+                    bCryptPasswordEncoder.encode(request.getPassword()))
+            );
+
+            return SignUpResponse.from(user);
+        }
+
     }
-
-}
