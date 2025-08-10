eaf7cd8201f49253e45a1ebc32898a214cd9be1c
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-10 09:10:49 +0900

    feat(#99) : 회원가입 서비스 추가

--- src/main/java/odyssey/backend/user/service/SignUpService.java
+++ src/main/java/odyssey/backend/user/service/SignUpService.java
@@ -1,11 +1,12 @@
     package odyssey.backend.user.service;
 
     import lombok.RequiredArgsConstructor;
+    import odyssey.backend.user.domain.Role;
     import odyssey.backend.user.domain.UserRepository;
     import odyssey.backend.user.domain.User;
     import odyssey.backend.user.dto.request.SignUpRequest;
     import odyssey.backend.user.dto.response.SignUpResponse;
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
     import org.springframework.stereotype.Service;
 
     @RequiredArgsConstructor
@@ -13,13 +14,14 @@
     public class SignUpService {
 
         private final UserRepository userRepository;
-        private final BCryptPasswordEncoder bCryptPasswordEncoder;
+        private final PasswordEncoder bCryptPasswordEncoder;
 
         public SignUpResponse signUp(SignUpRequest request) {
             User user = userRepository.save(User.from(
                     request,
-                    bCryptPasswordEncoder.encode(request.getPassword()))
-            );
+                    bCryptPasswordEncoder.encode(request.getPassword()),
+                    Role.USER
+            ));
 
             return SignUpResponse.from(user);
         }
