7be11b1ce944f1b0c2fed7ee82f1968d6d097976
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-10 09:49:33 +0900

    feat(#99) : 로그아웃 컨트롤러 작성

--- src/main/java/odyssey/backend/user/controller/AuthController.java
+++ src/main/java/odyssey/backend/user/controller/AuthController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import odyssey.backend.global.jwt.dto.response.TokenResponse;
 import odyssey.backend.global.response.CommonResponse;
 import odyssey.backend.global.response.SingleCommonResponse;
+import odyssey.backend.user.domain.User;
 import odyssey.backend.user.dto.request.LoginRequest;
 import odyssey.backend.user.dto.request.SignUpRequest;
 import odyssey.backend.user.dto.response.SignUpResponse;
@@ -13,6 +14,7 @@ import odyssey.backend.user.service.LogoutService;
 import odyssey.backend.user.service.RefreshService;
 import odyssey.backend.user.service.SignUpService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -40,5 +42,11 @@ public class AuthController {
     @GetMapping("/refresh")
     public SingleCommonResponse<TokenResponse> refreshAccessToken(@RequestHeader("Refresh-Token") String refreshToken) {
         return CommonResponse.ok(refreshService.refreshToken(refreshToken));
+    }
+
+    @GetMapping("/logout")
+    public SingleCommonResponse<String> logout(@AuthenticationPrincipal User user) {
+        logoutService.logout(user);
+        return CommonResponse.ok("로그아웃되었습니다.");
     }
 }
