a3cbe1f09fbf3b8a608192d1cf4dd91faf7ae471
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-10 09:24:39 +0900

    feat(#99) : 유저 레포지토리 작성

--- src/main/java/odyssey/backend/user/domain/UserRepository.java
+++ src/main/java/odyssey/backend/user/domain/UserRepository.java
@@ -3,7 +3,10 @@ package odyssey.backend.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
+    User findUserByUuid(Long uuid);
 }
