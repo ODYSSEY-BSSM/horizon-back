d3a73c2bb3a7d1c0a72afd7d505e3d483d79db53
leegunwoooo <leegunwoo0325@gmail.com>
2025-08-10 09:26:22 +0900

    feat(#99) : 리프레시 토큰 레포지토리 작성

---
+++ src/main/java/odyssey/backend/global/jwt/domain/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package odyssey.backend.global.jwt.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUuid(Long uuid);
+}
