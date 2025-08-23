ec24dd4b47edf0bb48546a5a1c6159570b87b204
leegunwoo <leegunwoo0325@gmail.com>
2025-08-24 07:51:31 +0900

    refactor(#119) : s3로 수정

--- src/main/java/odyssey/backend/global/config/S3Config.java
+++ src/main/java/odyssey/backend/global/config/S3Config.java
@@ -2,29 +2,32 @@ package odyssey.backend.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.global.S3Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
+@RequiredArgsConstructor
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+    private final S3Properties s3Properties;
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
+                s3Properties.getAccessKey(),
+                s3Properties.getSecretKey()
+        );
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion("ap-northeast-2")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .enablePathStyleAccess()
                 .build();
