4fd8c75b4395488df9b930bcf9db032c731a4dd7
leegunwoo <leegunwoo0325@gmail.com>
2025-08-24 06:33:18 +0900

    refactor(#119) : 유후 필요 없는 디렉토리 모두 삭제

--- src/main/java/odyssey/backend/image/exception/ImageSaveException.java
+++
@@ -1,4 +0,0 @@
-package odyssey.backend.image.exception;
-
-public class ImageSaveException extends RuntimeException {
-}
--- src/main/java/odyssey/backend/image/exception/InvalidImageFormatException.java
+++
@@ -1,4 +0,0 @@
-package odyssey.backend.image.exception;
-
-public class InvalidImageFormatException extends RuntimeException {
-}
