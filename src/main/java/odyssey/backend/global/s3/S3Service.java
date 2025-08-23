990f3c1c5ef0bfdaa0f6b6d242a52a116f093b2c
leegunwoo <leegunwoo0325@gmail.com>
2025-08-24 06:29:38 +0900

    feat(#119) : s3 서비스 코드 작성

---
+++ src/main/java/odyssey/backend/global/s3/S3Service.java
@@ -0,0 +1,39 @@
+package odyssey.backend.global.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 s3;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        s3.putObject(bucket, fileName, file.getInputStream(), metadata);
+        return fileName;
+    }
+
+    public void deleteFile(String fileName) {
+        s3.deleteObject(bucket, extractFileName(fileName));
+    }
+
+    private String extractFileName(String url) {
+        return url.substring(url.indexOf(".amazonaws.com/") + 14);
+    }
+
+}
