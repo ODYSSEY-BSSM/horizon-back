410f127441fda87ddb0c924b3707c8340f8a29e0
leegunwoo <leegunwoo0325@gmail.com>
2025-08-25 13:53:34 +0900

    feat(#120) : 팀 아이디로 팀을 찾는 메서드 추가

--- src/main/java/odyssey/backend/team/service/TeamService.java
+++ src/main/java/odyssey/backend/team/service/TeamService.java
@@ -2,6 +2,7 @@ package odyssey.backend.team.service;
 
 import lombok.RequiredArgsConstructor;
 import odyssey.backend.team.domain.Team;
+import odyssey.backend.team.domain.TeamApplyRepository;
 import odyssey.backend.team.domain.TeamRepository;
 import odyssey.backend.team.dto.request.TeamRequest;
 import odyssey.backend.team.dto.response.TeamResponse;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final TeamApplyRepository teamApplyRepository;
 
     public TeamResponse create(TeamRequest request, User leader){
         return TeamResponse.from(teamRepository.save(
@@ -25,6 +27,12 @@ public class TeamService {
                 .orElseThrow(() -> new IllegalArgumentException("팀은 팀장만 삭제할 수 있읍니다."));
 
         teamRepository.delete(team);
+    }
+
+    public TeamResponse findById(Long teamId) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 팀입니다."));
+        return TeamResponse.from(team);
     }
 
 }
