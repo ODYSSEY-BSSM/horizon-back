71ff0e190811868a00b8bcf0501f943c1b058894
leegunwoo <leegunwoo0325@gmail.com>
2025-08-25 10:50:17 +0900

    feat(#120) : 팀 삭제 로직 작성

--- src/main/java/odyssey/backend/team/domain/TeamRepository.java
+++ src/main/java/odyssey/backend/team/domain/TeamRepository.java
@@ -2,5 +2,8 @@ package odyssey.backend.team.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByLeader(String leader);
 }
