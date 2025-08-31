7e4a671e0832aa2327ab4dbad3d39730fb00b1ab
leegunwoo <leegunwoo0325@gmail.com>
2025-09-01 00:26:23 +0900

    refactor(#130) : 레포지토리 매개변수 변경

--- src/main/java/odyssey/backend/team/domain/TeamRepository.java
+++ src/main/java/odyssey/backend/team/domain/TeamRepository.java
@@ -1,9 +1,10 @@
 package odyssey.backend.team.domain;
 
+import odyssey.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    Optional<Team> findByLeader(String leader);
+    Optional<Team> findByLeader(User leader);
 }
