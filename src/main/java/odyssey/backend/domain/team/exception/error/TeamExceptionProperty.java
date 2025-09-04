0a36e29e7b00a87c06ca2666cc998223d23b42cb
leegunwoo <leegunwoo0325@gmail.com>
2025-09-05 00:06:44 +0900

    refactor(#145) : 팀 못찾을 때 예외처리

---
+++ src/main/java/odyssey/backend/domain/team/exception/error/TeamExceptionProperty.java
@@ -0,0 +1,15 @@
+package odyssey.backend.domain.team.exception.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import odyssey.backend.shared.exception.ErrorProperty;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TeamExceptionProperty implements ErrorProperty {
+    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "팀이 존재하지 않습니다");
+
+    private final HttpStatus status;
+    private final String message;
+}
