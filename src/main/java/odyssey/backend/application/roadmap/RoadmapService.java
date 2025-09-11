1d8c6fb98a2314816e4be2c09862600e35a4c1b8
leegunwoo <leegunwoo0325@gmail.com>
2025-09-11 17:55:08 +0900

    refactor(#154) : 팀 로드맵, 개인로드맵 메서드 분리

--- src/main/java/odyssey/backend/application/roadmap/RoadmapService.java
+++ src/main/java/odyssey/backend/application/roadmap/RoadmapService.java
@@ -2,15 +2,16 @@
 
     import lombok.RequiredArgsConstructor;
     import lombok.extern.slf4j.Slf4j;
-    import odyssey.backend.infrastructure.s3.S3Service;
-    import odyssey.backend.presentation.roadmap.dto.response.ImageUrlResponse;
+    import odyssey.backend.domain.auth.User;
+    import odyssey.backend.domain.roadmap.Roadmap;
     import odyssey.backend.domain.roadmap.exception.RoadmapNotFoundException;
     import odyssey.backend.infrastructure.persistence.roadmap.RoadmapRepository;
+    import odyssey.backend.infrastructure.s3.S3Service;
+    import odyssey.backend.presentation.roadmap.dto.response.ImageUrlResponse;
+    import odyssey.backend.presentation.roadmap.dto.response.PersonalRoadmapResponse;
     import odyssey.backend.presentation.roadmap.dto.response.RoadmapCountResponse;
-    import odyssey.backend.presentation.roadmap.dto.response.RoadmapResponse;
-    import odyssey.backend.domain.auth.User;
+    import odyssey.backend.presentation.roadmap.dto.response.TeamRoadmapResponse;
     import org.springframework.stereotype.Service;
-    import odyssey.backend.domain.roadmap.Roadmap;
 
     import java.util.List;
 
@@ -23,27 +24,27 @@
         private final S3Service s3Service;
 
 
-        public List<RoadmapResponse> findPersonalRoadmaps(User user) {
+        public List<PersonalRoadmapResponse> findPersonalRoadmaps(User user) {
             return roadmapRepository.findByUserAndTeamIdIsNullOrderByLastAccessedAtDesc(user).stream()
-                    .map(roadmap -> RoadmapResponse.from(roadmap, user.getUuid()))
+                    .map(roadmap -> PersonalRoadmapResponse.from(roadmap, user.getUuid()))
                     .toList();
         }
 
-        public List<RoadmapResponse> findTeamRoadmaps(User user, Long teamId) {
+        public List<TeamRoadmapResponse> findTeamRoadmaps(User user, Long teamId) {
             return roadmapRepository.findByTeamIdOrderByLastAccessedAtDesc(teamId)
                     .stream()
-                    .map(roadmap -> RoadmapResponse.from(roadmap, user.getUuid()))
+                    .map(roadmap -> TeamRoadmapResponse.from(roadmap, user.getUuid()))
                     .toList();
         }
 
-        public RoadmapResponse getLastAccessedRoadmap(User user) {
+        public PersonalRoadmapResponse getLastAccessedRoadmap(User user) {
 
             Roadmap roadmap = roadmapRepository.findTopByUserOrderByLastAccessedAtDesc(user)
                     .orElseThrow(RoadmapNotFoundException::new);
 
             log.info("마지막 접속 로드맵 Id : {}", roadmap.getId());
 
-            return RoadmapResponse.from(roadmap, user.getUuid());
+            return PersonalRoadmapResponse.from(roadmap, user.getUuid());
         }
 
         public RoadmapCountResponse getRoadmapCount(User user) {
