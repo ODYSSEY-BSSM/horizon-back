44008e84f7f3a8761e69fa1e3cd0760d3f08ce99
leegunwoo <leegunwoo0325@gmail.com>
2025-09-11 17:31:26 +0900

    refactor(#154) : 팀 로드맵 응답 분리

---
+++ src/main/java/odyssey/backend/presentation/roadmap/dto/response/TeamRoadmapResponse.java
@@ -0,0 +1,37 @@
+package odyssey.backend.presentation.roadmap.dto.response;
+
+import odyssey.backend.domain.roadmap.Roadmap;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record TeamRoadmapResponse(
+        Long id,
+        String title,
+        String description,
+        List<String> categories,
+        String thumbnailUrl,
+        LocalDate lastModifiedAt,
+        LocalDateTime lastAccessedAt,
+        boolean isFavorite,
+        Long uuid,
+        Long teamId,
+        String teamName
+) {
+    public static TeamRoadmapResponse from(Roadmap roadmap, Long uuid){
+        return new TeamRoadmapResponse(
+                roadmap.getId(),
+                roadmap.getTitle(),
+                roadmap.getDescription(),
+                roadmap.getCategories(),
+                roadmap.getImageUrl(),
+                roadmap.getLastModifiedAt(),
+                roadmap.getLastAccessedAt(),
+                roadmap.getIsFavorite(),
+                uuid,
+                roadmap.getTeam().getId(),
+                roadmap.getTeam().getName()
+        );
+    }
+}
