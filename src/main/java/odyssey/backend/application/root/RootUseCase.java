a07504872ff77ed67391e4b91395e5bc91b26a38
leegunwoo <leegunwoo0325@gmail.com>
2025-09-14 20:15:58 +0900

    refactor(#156) : 최상단엔 무조건 디렉토리만 존재하도록 수정

--- src/main/java/odyssey/backend/application/root/RootUseCase.java
+++ src/main/java/odyssey/backend/application/root/RootUseCase.java
@@ -3,11 +3,9 @@ package odyssey.backend.application.root;
 import lombok.RequiredArgsConstructor;
 import odyssey.backend.domain.auth.User;
 import odyssey.backend.domain.directory.Directory;
-import odyssey.backend.domain.roadmap.Roadmap;
 import odyssey.backend.infrastructure.persistence.directory.DirectoryRepository;
 import odyssey.backend.infrastructure.persistence.roadmap.RoadmapRepository;
 import odyssey.backend.presentation.directory.dto.response.DirectoryResponse;
-import odyssey.backend.presentation.roadmap.dto.response.SimpleRoadmapResponse;
 import odyssey.backend.presentation.root.dto.response.RootContentResponse;
 import org.springframework.stereotype.Service;
 
@@ -22,32 +20,22 @@ public class RootUseCase {
 
     public RootContentResponse getRootContents(User user) {
         List<Directory> rootDirectories = directoryRepository.findByParentIsNullAndUser(user);
-        List<Roadmap> rootRoadmaps = roadmapRepository.findByDirectoryIsNullAndUser(user);
 
         List<DirectoryResponse> directoryResponses = rootDirectories.stream()
                 .map(DirectoryResponse::from)
                 .toList();
 
-        List<SimpleRoadmapResponse> roadmapResponses = rootRoadmaps.stream()
-                .map(SimpleRoadmapResponse::from)
-                .toList();
-
-        return RootContentResponse.from(directoryResponses, roadmapResponses);
+        return RootContentResponse.from(directoryResponses);
     }
 
     public RootContentResponse getTeamRootContents(User user, Long teamId) {
         List<Directory> rootDirectories = directoryRepository.findByParentIsNullAndTeamId(teamId);
-        List<Roadmap> rootRoadmaps = roadmapRepository.findByDirectoryIsNullAndTeamId(teamId);
 
         List<DirectoryResponse> directoryResponses = rootDirectories.stream()
                 .map(DirectoryResponse::from)
                 .toList();
 
-        List<SimpleRoadmapResponse> roadmapResponses = rootRoadmaps.stream()
-                .map(SimpleRoadmapResponse::from)
-                .toList();
-
-        return RootContentResponse.from(directoryResponses, roadmapResponses);
+        return RootContentResponse.from(directoryResponses);
     }
 
 }
