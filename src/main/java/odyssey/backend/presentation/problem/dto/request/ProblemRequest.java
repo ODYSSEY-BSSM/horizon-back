abea1970f2567aca62a3a2b6622567711f7138de
leegunwoo <leegunwoo0325@gmail.com>
2025-09-15 09:45:38 +0900

    test(#157) : 테스트 코드용 생성자 추가

--- src/main/java/odyssey/backend/presentation/problem/dto/request/ProblemRequest.java
+++ src/main/java/odyssey/backend/presentation/problem/dto/request/ProblemRequest.java
@@ -1,9 +1,11 @@
 package odyssey.backend.presentation.problem.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProblemRequest {
 
     @NotBlank(message = "필수값입니다.")
--- src/main/java/odyssey/backend/presentation/problem/dto/request/SolveProblemRequest.java
+++ src/main/java/odyssey/backend/presentation/problem/dto/request/SolveProblemRequest.java
@@ -1,9 +1,13 @@
 package odyssey.backend.presentation.problem.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolveProblemRequest {
 
     @NotBlank(message = "필수값입니다.")
