3a1dea3f7571e8dec302fc2f648301760c27144f
leegunwoo <leegunwoo0325@gmail.com>
2025-09-17 08:10:04 +0900

    refactor(#176) : 쿠키에서 뽑아내도록 수정

--- src/main/java/odyssey/backend/infrastructure/jwt/JWTAuthenticationFilter.java
+++ src/main/java/odyssey/backend/infrastructure/jwt/JWTAuthenticationFilter.java
@@ -4,16 +4,17 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import odyssey.backend.infrastructure.jwt.exception.InvalidTokenTypeException;
-import odyssey.backend.infrastructure.jwt.service.TokenService;
 import odyssey.backend.domain.auth.User;
+import odyssey.backend.infrastructure.jwt.exception.InvalidTokenTypeException;
+import odyssey.backend.infrastructure.jwt.exception.TokenNotFoundException;
+import odyssey.backend.infrastructure.jwt.service.TokenService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -73,10 +74,15 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
+                if("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
         }
-        return null;
+        throw new TokenNotFoundException();
     }
+
 }
