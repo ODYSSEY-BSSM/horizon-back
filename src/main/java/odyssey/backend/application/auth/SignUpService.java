303212f8a611fdc14fcd46d32c5618d6dad533d9
leegunwoo <leegunwoo0325@gmail.com>
2025-09-18 00:54:31 +0900

    feat(#184) : 인증되 이메일만 회원가입 되도록 검증 메서드 작성

--- src/main/java/odyssey/backend/application/auth/SignUpService.java
+++ src/main/java/odyssey/backend/application/auth/SignUpService.java
@@ -2,8 +2,10 @@
 
     import lombok.RequiredArgsConstructor;
     import odyssey.backend.domain.auth.Role;
-    import odyssey.backend.infrastructure.persistence.auth.UserRepository;
+    import odyssey.backend.domain.auth.SignUpVerification;
     import odyssey.backend.domain.auth.User;
+    import odyssey.backend.infrastructure.persistence.auth.SignUpVerificationRepository;
+    import odyssey.backend.infrastructure.persistence.auth.UserRepository;
     import odyssey.backend.presentation.auth.dto.request.SignUpRequest;
     import odyssey.backend.presentation.auth.dto.response.SignUpResponse;
     import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,8 +17,11 @@
 
         private final UserRepository userRepository;
         private final PasswordEncoder bCryptPasswordEncoder;
+        private final SignUpVerificationRepository signUpVerificationRepository;
 
         public SignUpResponse signUp(SignUpRequest request) {
+            validate(request.getEmail());
+
             User user = userRepository.save(User.from(
                     request,
                     bCryptPasswordEncoder.encode(request.getPassword()),
@@ -24,6 +29,17 @@
             ));
 
             return SignUpResponse.from(user);
+        }
+
+        public void validate(String email) {
+            SignUpVerification signUpVerification = signUpVerificationRepository.findById(email)
+                    .orElseThrow(() -> new IllegalArgumentException("잘못된 이메일"));
+
+            if(!signUpVerification.isVerified()){
+                throw new IllegalArgumentException("인증되지 않은 사용자입니다.");
+            }
+
+            signUpVerificationRepository.delete(signUpVerification);
         }
 
     }
