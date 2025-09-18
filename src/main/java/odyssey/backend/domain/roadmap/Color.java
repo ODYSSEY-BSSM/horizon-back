c2bc56ebfd6cdd705abc8198fddc9a1caf4216af
leegunwoo <leegunwoo0325@gmail.com>
2025-09-18 17:44:10 +0900

    refactor(#187) : 색깔 이넘 클래스 작성

---
+++ src/main/java/odyssey/backend/domain/roadmap/Color.java
@@ -0,0 +1,17 @@
+package odyssey.backend.domain.roadmap;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Color {
+    RED("DC2626"),
+    ORANGE("EA580C"),
+    YELLOW("E6C200"),
+    GREEN("16A34A"),
+    BLUE("2666DC"),
+    PURPLE("A826DC");
+
+    private final String description;
+}
